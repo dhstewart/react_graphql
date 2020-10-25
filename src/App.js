fb9c7477bff8198ffb875b71b3e83751b4247816
Justin Aiello <justin@macbook-pro.attlocal.net>
2020-10-25 12:06:49 -0400

    chore: fix formatting in dashboard and app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react"
+import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import Dashboard from "./modules/dashboard"
+import Dashboard from './modules/dashboard';
 
 const link = createHttpLink({
-  uri: 'http://localhost:3000/graphql'
+  uri: 'http://localhost:3000/graphql',
 });
 
 const client = new ApolloClient({
   link: link,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 function App() {
--- src/modules/dashboard/Dashboard.js
+++ src/modules/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -15,19 +15,20 @@ const CHARACTER_QUERY = gql`
 function Dashboard() {
   return (
     <Query query={CHARACTER_QUERY}>
-      {({ loading, error, data })=> {
-        if (loading) return <div>Fetching..</div>
-        if (error) return console.log("my great error: ", error);
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching..</div>;
+        if (error) return <div>Error</div>;
+
         return (
           <div>
-           {data.fetchCharacters.map((item)=> (
-             <div key={item.id}>{item.name}</div>
-           ))}
+            {data.fetchCharacters.map((item) => (
+              <div key={item.id}>{item.name}</div>
+            ))}
           </div>
-        )
+        );
       }}
-      </Query>
-  )
+    </Query>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
